e68b60286519b15a1c6ffe75fd62d5e0fc635959
degel55 <chicubfan@gmail.com>
2016-08-24 17:34:38 -0500

    update to store_item

--- store_item.rb
+++ store_item.rb
@@ -1,38 +1,31 @@
 class Item
+  attr_reader :sport, :size, :cost
+
   def initialize(input_item_hash)
     @sport = input_item_hash[:sport]
     @size = input_item_hash[:size]
     @cost = input_item_hash[:cost]
-  end
-
-  def sport
-    @sport
   end 
-  
-  def size
-    @size
-  end 
-
-  def cost
-    @cost
-  end  
 end  
 
 class Food < Item
-
   def initialize(input_item_hash)
     super(input_item_hash)
-    @shelf_life = item_attribute_hash[:shelf_life]
+    @shelf_life = input_item_hash[:shelf_life]
   end
-
 end
 
 first_item = Item.new({sport: 'soccer', size: '15', cost: 10})
 second_item = Item.new({sport: 'basketball', size: 'female', cost: 15})
 third_item = Item.new({sport: 'football', size: 'peewee', cost: 20})
+fourth_item = Food.new({sport: 'golf', size: 'regular', cost: 35, shelf_life: 'one hole'})
 
 puts first_item.sport
 puts first_item.size
 puts first_item.cost
+puts fourth_item.sport
+puts fourth_item.size
+puts fourth_item.cost
+puts fourth_item.shelf_life
 
 
--- weather_data.rb
+++ weather_data.rb
@@ -2,4 +2,3 @@ weather_data = File.open('./weather.dat', File::RDONLY){|f| f.read }
 
 p weather_data
 
-we
